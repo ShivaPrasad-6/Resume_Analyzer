0c1af29abcd99911f5a7a8256eed40c7f9a6e462
ShivaPrasad-6 <shivaprasadbandarupally68@gmail.com>
2026-03-28 18:40:49 +0530

    wipe data

--- app/routes.ts
+++ app/routes.ts
@@ -6,4 +6,5 @@ export default [
   route('/upload', 'routes/upload.tsx'),
   route('/resume/:id', 'routes/resume.tsx'),
   route('/resume/:id/feedback', 'routes/feedback.tsx'),
+  route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
